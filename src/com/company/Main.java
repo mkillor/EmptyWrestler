f4c01b544f9ea6933fa3f8c96ae3172ade696199
mak1115 <mak1115@swcsd.us>
2022-02-10 10:30:17 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+
+interface Wrestler{
+    int salary = 0;
+
+    void aButton();
+    void bButton();
+    void specialMove();
+    void entranceMusic();
+
+
+}
+
+
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
